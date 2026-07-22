e591a3f4d7f3961cb082608391e2c4bef9957eec
T1mC0de <tim12535@gmail.com>
2026-07-22 18:18:15 +0300

    server

---
+++ internal/server/server.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"build-your-own-reverse-proxy/internal/config"
+	"net/http"
+	"context"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func NewServer(cfg *config.Config, handler http.Handler) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:    cfg.Server.Port,
+			Handler: handler,
+		},
+	}
+}
+
+func (s *Server) StartServer() error {
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) StopServer(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
