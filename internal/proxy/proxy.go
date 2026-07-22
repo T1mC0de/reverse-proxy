b81d1750ff7947c278b2277f188afe2c380ca48b
T1mC0de <tim12535@gmail.com>
2026-07-22 18:11:52 +0300

    proxy itself

---
+++ internal/proxy/proxy.go
@@ -0,0 +1,44 @@
+package proxy
+
+import (
+	"build-your-own-reverse-proxy/internal/router"
+	"build-your-own-reverse-proxy/internal/forwarder"
+	"log/slog"
+	"net/http"
+	"context"
+)
+
+type Proxy struct {
+	router    *router.Router
+	forwarder *forwarder.Forwarder
+	logger    *slog.Logger
+}
+
+func NewProxy(router *router.Router, forwarder *forwarder.Forwarder, logger *slog.Logger) *Proxy {
+	return &Proxy{
+		router:    router,
+		forwarder: forwarder,
+		logger:    logger,
+	}
+}
+
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	route := p.router.Match(r.URL.Path)
+	if route == nil {
+		p.logger.Debug("Route not found", "path", r.URL.Path)
+		http.Error(w, "Route not found", http.StatusNotFound)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(r.Context(), route.Timeout)
+	defer cancel()
+
+	err := p.forwarder.Forward(ctx, w, r, route.Upstreams[0].URL)
+	if err != nil {
+		p.logger.Error("Failed to forward request", "error", err)
+		http.Error(w, "Failed to forward request", http.StatusBadGateway)
+		return
+	}
+}
+
+
