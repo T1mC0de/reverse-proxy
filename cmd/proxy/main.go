5ece55ca563fad70c53d38d52bbe3bbe556322eb
T1mC0de <tim12535@gmail.com>
2026-07-23 12:55:12 +0300

    builded

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"build-your-own-reverse-proxy/internal/config"
+	"build-your-own-reverse-proxy/internal/runtime"
+	"build-your-own-reverse-proxy/internal/server"
+	"github.com/lmittmann/tint"
+	"log/slog"
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	logger := slog.New(tint.NewTextHandler(os.Stdout, &tint.Options{Level: slog.LevelInfo, TimeFormat: time.Kitchen}))
+	slog.SetDefault(logger)
 
-	mux.HandleFunc("/", handler)
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
+	cfg, err := config.Load("configs/config.yaml")
+	if err != nil {
+		logger.Error("Failed to load config", "error", err)
+		os.Exit(1)
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
-	}
-}
+	logger.Info("Config loaded", "config", cfg)
 
-func handler(w http.ResponseWriter, r *http.Request) {
+	rt, err := runtime.Build(cfg, logger)
+	if err != nil {
+		logger.Error("Failed to create runtime", "error", err)
+		os.Exit(1)
+	}
+
+	logger.Info("Runtime built successfully")
+
+	srv := server.NewServer(cfg, rt.Proxy)
+
+	logger.Info("Server starting")
+	if err := srv.StartServer(); err != nil && err != http.ErrServerClosed {
+		logger.Error("Failed to start server", "error", err)
+		os.Exit(1)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module build-your-own-reverse-proxy
 
 go 1.26.3
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/lmittmann/tint v1.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
